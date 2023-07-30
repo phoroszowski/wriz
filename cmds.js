52585d8fca8d6f94b7549585ede81f05f55c43c5
Peter Horoszowski <phoroszowski@gmail.com>
2023-07-29 20:55:32 -0400

    additions for rss gpio class

---
+++ cmds.js
@@ -0,0 +1,75 @@
+const { program } = require('commander');
+var mpd = require('mpd');
+const gpio = require('./lib/gpio.js')
+let mpdCmds = mpd.cmd;
+
+
+program.command('getFeed')
+    .argument('<rss>', 'RSS feed url')
+    .action((feedUrl,options) => {
+        console.log(`Fetching: ${feedUrl}`);
+        console.log(options);
+
+        let Parser = require('rss-parser');
+        let parser = new Parser();
+
+        (async () => {
+
+        let feed = await parser.parseURL(feedUrl);
+        console.log(feed.title);
+
+        console.log(feed.items[13].title);
+        console.log(feed.items[13].enclosure.url);
+        let currentMode = "uninitialized";
+        var client = mpd.connect({
+            port: 6600,
+            host: 'localhost',
+          });
+          client.on('ready', function() {
+            console.log("ready");
+            client.sendCommand(mpdCmds("clear", []), function(err, msg) {
+                if (err) throw err;
+                console.log(msg);
+                client.sendCommand(mpdCmds("add"  ,[feed.items[13].enclosure.url]), function(err, msg) {
+                    console.log(msg);
+                    client.sendCommand(mpdCmds("play",[]), function(err, msg) {
+                        console.log(msg);
+                    });
+                    currentMode = "playing";
+
+                });
+          });
+        });
+
+        let gW = new gpio.gpioWrapper();
+        gW.listen(function(channel, value) {
+            if(currentMode == "playing")
+            {
+                client.sendCommand(mpdCmds("pause",[]), function(err, msg) {
+                console.log(msg);
+                });
+            }
+            else{
+                client.sendCommand(mpdCmds("play",[]), function(err, msg) {
+                    console.log(msg);
+                });
+            }
+                
+            });
+
+        console.log('at the end');
+    })();
+});
+
+program.command('testGpioButtons')
+    .action((feedUrl,options) => {
+        let gW = new gpio.gpioWrapper();
+        gW.listen(function(channel, value) {
+            console.log('Channel ' + channel + ' value is now ' + value);
+        });
+
+    })
+
+    
+program.parse();
+
